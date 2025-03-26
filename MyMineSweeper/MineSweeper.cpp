be6e2cf7a8be8ae6140ed9b33c358ffb7f542ab3
가현 <bufar1014@gmail.com>
2025-03-27 02:58:01 +0900

    flag() 함수와 getLocation() 함수 작성
    Open할 좌표를 입력받고 FlagMode라면 깃발을 표시할 좌표를 받는다

--- MyMineSweeper/MineSweeper.cpp
+++ MyMineSweeper/MineSweeper.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <ctype.h>
 
 // 상태(x,y) : Open/Hide/Flag
 // 인접지뢰수(x,y) : 0~8, 9는 지뢰를 나타낸다.
@@ -27,6 +30,8 @@ int& nearMine(int x, int y) { return MapMine[y][x]; }
 bool isValid(int x, int y) {
     return x < 9 && x >= 0 && y < 9 && y >= 0;
 }
+
+// 입력받은 좌표를 여는 함수
 void dig(int x, int y)
 {
     // 해당 좌표가 열리지 않았다면
@@ -43,4 +48,38 @@ void dig(int x, int y)
             }
         }
     }
+}
+
+// 입력받은 좌표에 깃발을 꽂는 함수
+void flag(int x, int y) {
+    if (isValid(x, y) && state(x, y) == Hide) {
+        state(x, y) = Flag;
+    }
+}
+
+// Flag 좌표를 받는 모드라면 true를 반환하고 Open 좌표를 받는 경우는 false를 반환한다. - bool
+bool getLocation(int& x, int& y) {
+    printf("\n(깃발을 꽂을 위치를 선택하려면 'F'를 추가 입력하세요)\n");
+    printf("행(A~I)과 열(1~9)을 입력하세요 : \n");
+    printf("입력 : ");
+
+    // 'F' 입력 시 깃발 모드(true)로 전환
+    bool isFlagMode = false;
+
+    // 입력받은 문자를 무조건 대문자로 변환 후 'A'를 빼서 y 좌표의 값을 구한다.
+    y = toupper(_getch()) - 'A';
+
+    // 'F'를 입력받은 경우
+    if (y == 'F' - 'A') {
+        // 깃발 모드로 전환
+        isFlagMode = true;
+
+        // 입력받은 첫 번째 문자를 isFlagMode 변경에 사용했기 때문에 다시 y 좌표 입력받기
+        y = toupper(_getche()) - 'A';
+    }
+
+    // x 좌표 입력받기
+    x = _getch() - '1';
+
+    return isFlagMode;
 }
